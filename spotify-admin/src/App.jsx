d1dc023c1a4a1febe78f7689aa8079082ca5ad37
SAHILKARN33 <104265862+SAHILKARN33@users.noreply.github.com>
2024-08-09 11:31:35 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -9,6 +9,7 @@ import ListAlbum from './pages/ListAlbum/ListAlbum';
 import Sidebar from './components/Sidebar/Sidebar';
 import Navbar from './components/Navbar/Navbar';
 
+export const url='https://sppotiifyfull-backend.onrender.com';
 
 const App = () => {
   return (
